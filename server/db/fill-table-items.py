110c068de6a5f1c915edc93fc7b133af70809a7a
starshynova <l.starshynova@gmail.com>
2025-06-06 21:06:22 +0200

    server: filled table items

---
+++ server/db/fill-table-items.py
@@ -0,0 +1,61 @@
+import uuid
+from connectDB import conn
+
+items = [
+    {
+        "id": uuid.uuid4(),
+        "title": "Black pants",
+        "price": 59,
+        "description": "Black wide pants for ladies",
+        "main_photo_url": "https://e-commerce-img.s3.eu-north-1.amazonaws.com/img-01.jpg"
+    },
+    {
+        "id": uuid.uuid4(),
+        "title": "Burgundy pants",
+        "price": 69,
+        "description": "Burgundy wide pants for ladies",
+        "main_photo_url": "https://e-commerce-img.s3.eu-north-1.amazonaws.com/img-02.jpg"
+    },
+    {
+        "id": uuid.uuid4(),
+        "title": "Blue shirt and pants",
+        "price": 89,
+        "description": None,
+        "main_photo_url": "https://e-commerce-img.s3.eu-north-1.amazonaws.com/img-03.jpg"
+    },
+{
+        "id": uuid.uuid4(),
+        "title": "Beige shirt and pants",
+        "price": 99,
+        "description": "Set of shirt and pants",
+        "main_photo_url": "https://e-commerce-img.s3.eu-north-1.amazonaws.com/img-04.jpg"
+    },
+{
+        "id": uuid.uuid4(),
+        "title": "White shirt and skirt",
+        "price": 69,
+        "description": "White shirt and midi-skirt",
+        "main_photo_url": "https://e-commerce-img.s3.eu-north-1.amazonaws.com/img-05.jpg"
+    },
+{
+        "id": uuid.uuid4(),
+        "title": "White dress",
+        "price": 129,
+        "description": "Nice white dress",
+        "main_photo_url": "https://e-commerce-img.s3.eu-north-1.amazonaws.com/img-06.jpg"
+    },
+]
+
+cursor = conn.cursor()
+
+for item in items:
+    cursor.execute("""
+        INSERT INTO items (id, title, price, description, main_photo_url)
+        VALUES (%s, %s, %s, %s, %s)
+    """, (str(item["id"]), item["title"], item["price"], item["description"], item["main_photo_url"]))
+
+conn.commit()
+cursor.close()
+conn.close()
+
+print("✅ The items table is filled with initial data..")
