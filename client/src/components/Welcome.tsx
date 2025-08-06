b574bba09427614b06e5533ea6a3e8fa4fdd859c
starshynova <l.starshynova@gmail.com>
2025-08-06 16:55:24 +0200

    changing user icon if login

--- client/src/components/Welcome.tsx
+++ client/src/components/Welcome.tsx
@@ -1,15 +1,17 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 
 const Welcome = () =>{
     const navigate = useNavigate();
+    const { login } = useAuth();
 
     useEffect(() => {
         const params = new URLSearchParams(window.location.search);
         const token = params.get("token");
 
         if (token) {
-            localStorage.setItem("token", token);
+            login(token);
             setTimeout(() => {
                 navigate("/");
             }, 200);
---
+++ client/src/utils/getUserRole.tsx
@@ -0,0 +1,12 @@
+const getUserRole = (): string | null => {
+  const token = localStorage.getItem("token");
+  if (!token) return null;
+  try {
+    const payload = JSON.parse(atob(token.split(".")[1]));
+    return payload.role;
+  } catch {
+    return null;
+  }
+}
+
+export default getUserRole;
