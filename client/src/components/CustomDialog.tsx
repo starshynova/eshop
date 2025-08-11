10dd29c1fd60a9c3e47d05923dd0fe9f25bdd5dd
starshynova <l.starshynova@gmail.com>
2025-08-11 14:55:21 +0200

    designed cart page if user is not logged in

--- client/src/components/CustomDialog.tsx
+++ client/src/components/CustomDialog.tsx
@@ -22,19 +22,19 @@ const CustomDialog: React.FC<CustomDialogProps> = ({
       open={isOpen}
       onClose={onClose}
       as="div"
-      className="fixed inset-0 z-50 flex items-center justify-center"
+      className="fixed inset-0 z-50 flex items-center justify-center "
     >
       <div
         className="fixed inset-0 bg-black bg-opacity-50"
         aria-hidden="true"
       />
       <div className="fixed inset-0 flex items-center justify-center p-4">
-        <DialogPanel className="flex flex-col bg-white rounded-lg shadow-lg p-8 max-w-sm w-full justify-center items-center">
-          <p className="text-gray-700 mb-4">{message}</p>
+        <DialogPanel className="flex flex-col bg-white rounded-lg shadow-lg p-8 max-w-md w-full justify-center items-center">
+          <p className="text-black text-xl mb-16">{message}</p>
           <Button
+            className="w-full"
             onClick={() => {
               onClickButton();
-              onClose();
             }}
           >
             {buttonTitle}
