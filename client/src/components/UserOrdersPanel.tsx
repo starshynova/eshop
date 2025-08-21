d74f6b56da80b3aa2c9416ff448e5727c3abc7aa
starshynova <l.starshynova@gmail.com>
2025-08-21 15:03:51 +0200

    prettier

--- client/src/components/UserOrdersPanel.tsx
+++ client/src/components/UserOrdersPanel.tsx
@@ -114,48 +114,54 @@ const UserOrdersPanel: React.FC = () => {
   if (!orders.length) return <div>You don't have any orders yet.</div>;
 
   return (
-  <div className="w-full flex flex-col items-center gap-8">
-    {[...orders]
-      .sort((a, b) => new Date(b.created_at).getTime() - new Date(a.created_at).getTime())
-      .map((order) => (
-        <div
-          key={order.id}
-          className="w-[60%] rounded-sm border-2 border-gray-300 p-8 bg-white"
-        >
-          <div className="mb-4 pb-4 flex justify-between items-center border-b-[1px] border-b-gray-400">
-            <span className="text-lg font-bold">Order date</span>
-            <span className="text-gray-600">
-              {new Date(order.created_at).toLocaleString()}
-            </span>
+    <div className="w-full flex flex-col items-center gap-8">
+      {[...orders]
+        .sort(
+          (a, b) =>
+            new Date(b.created_at).getTime() - new Date(a.created_at).getTime(),
+        )
+        .map((order) => (
+          <div
+            key={order.id}
+            className="w-[60%] rounded-sm border-2 border-gray-300 p-8 bg-white"
+          >
+            <div className="mb-4 pb-4 flex justify-between items-center border-b-[1px] border-b-gray-400">
+              <span className="text-lg font-bold">Order date</span>
+              <span className="text-gray-600">
+                {new Date(order.created_at).toLocaleString()}
+              </span>
+            </div>
+            <div className="flex flex-col gap-4">
+              {order.items.map((item) => (
+                <button
+                  key={item.id}
+                  className="flex items-center gap-4 cursor-pointer"
+                  onClick={() => navigate(`/products/${item.id}`)}
+                >
+                  <img
+                    src={item.main_photo_url}
+                    alt={item.title}
+                    className="w-16 h-16 object-cover rounded"
+                  />
+                  <div className="flex-1 flex flex-col items-start">
+                    <div className="font-semibold">{item.title}</div>
+                    <div className="text-gray-600">
+                      Quantity: {item.quantity}
+                    </div>
+                  </div>
+                  <div className="font-bold">
+                    € {item.price * item.quantity}{" "}
+                  </div>
+                </button>
+              ))}
+            </div>
+            <div className="mt-4 text-right font-bold text-lg border-t-[1px] border-t-gray-400 pt-4">
+              Order Summary: € {order.total_price}
+            </div>
           </div>
-          <div className="flex flex-col gap-4">
-            {order.items.map((item) => (
-              <button
-                key={item.id}
-                className="flex items-center gap-4 cursor-pointer"
-                onClick={() => navigate(`/products/${item.id}`)}
-              >
-                <img
-                  src={item.main_photo_url}
-                  alt={item.title}
-                  className="w-16 h-16 object-cover rounded"
-                />
-                <div className="flex-1 flex flex-col items-start">
-                  <div className="font-semibold">{item.title}</div>
-                  <div className="text-gray-600">Quantity: {item.quantity}</div>
-                </div>
-                <div className="font-bold">€ {item.price * item.quantity} </div>
-              </button>
-            ))}
-          </div>
-          <div className="mt-4 text-right font-bold text-lg border-t-[1px] border-t-gray-400 pt-4">
-            Order Summary: € {order.total_price}
-          </div>
-        </div>
-      ))}
-  </div>
-);
-
+        ))}
+    </div>
+  );
 };
 
 export default UserOrdersPanel;
